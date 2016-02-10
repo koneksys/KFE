6071821cf4a7cfd84967522e8911037d915e1c0d
Jerome <jerome@git.de>
2016-02-10 18:12:48 +0000

    implementation point evaluation and firstderivative calculation

--- femaths/funspace.py
+++ femaths/funspace.py
@@ -9,8 +9,8 @@ from sympy import*
 from sympy.polys.monomials import itermonomials
 from scipy.special import comb
 from sympy.physics.quantum import TensorProduct
-from femaths.polytope import Polytopetype, Polytopedimension
-from femaths.funreq import Doftype
+from polytope import Polytopetype, Polytopedimension
+from funreq import Doftype
 from enum import Enum
 
 
@@ -36,12 +36,12 @@ class Funspace:
             else:
                 listdof = []
                 degreemax = 10
-                if polytopetype.name == Polytopetype.simplex.name:
+                if polytopetype == Polytopetype.simplex:
 
                     for i in range(0, degreemax):
                         listdof.append(comb(polytopedimension.value + i, i))
 
-                elif polytopetype.name == Polytopetype.cube.name:
+                elif polytopetype == Polytopetype.cube:
 
                     for i in range(0, degreemax):
                         listdof.append(pow(i+1, polytopedimension.value))
@@ -92,7 +92,6 @@ class Funspace:
                         for i in range(0, self.dimension):
                             listvar.append(listallvar[i])
 
-                        b = []
                         for i in range(0, self.dimension):
                             basisset = itermonomials([listvar[i]], self.degree)
                             basislist = []
@@ -116,23 +115,65 @@ class Funspace:
         else:
             raise NameError('wrong type entry. arg1: Polytopetype, arg2: Polytopedimension, arg3: Kform')
 
+    def funeval(self, doftype, *args):
 
-#    def getfun(self, doftype):
-#        if isinstance(doftype, Doftype):
-#            if doftype.name == Doftype.pointevaluation:
+        x, y, z = symbols('x y z')
+        listallvar =[x, y, z]
+        listvar = []
 
+        if isinstance(doftype, Doftype):
+            values = []
+            for elem in args:
+                values.append(elem)
 
+            if len(values) == self.dimension:
+                for i in range(0, self.dimension):
+                    listvar.append(listallvar[i])
+
+                if doftype == Doftype.pointevaluation:
+                    funeval = self.fun
+                    for i in range(0, len(values)):
+                        funeval = funeval.subs(listvar[i], values[i])
+
+                    return funeval
+
+                if doftype == Doftype.firstderivative:
+                    #evaluation can be multidimensional
+                    funeval = []
+                    #for each variable calculate the first derivative
+                    for i in range(0, len(values)):
+                        funevalitem = self.fun
+                        funevalitem = diff(funevalitem, listvar[i], 1)
+                        #for each first derivative calculate the derivative value
+                        for k in range(0, len(values)):
+                            funevalitem = funevalitem.subs(listvar[k], values[k])
+                        funeval.append(funevalitem)
+
+                    return funeval
+
+            else:
+                raise NameError('number of arguments is of same dimension as polytope')
+
+        else:
+            raise NameError('entry doftype has to be of type doftype')
 
 
 
 
 def main():
     polytopetype1 = Polytopetype.simplex
-    dimension1 = Polytopedimension.dim2
+    dimension1 = Polytopedimension.dim3
     kform1 = Kform.zeroform
-    dofnumber1 = 3
+    dofnumber1 = 10
+    doftype1 = Doftype.pointevaluation
+    doftype2 = Doftype.firstderivative
     funspace1 = Funspace(polytopetype1, dimension1, kform1, dofnumber1)
     print(funspace1.__dict__)
+    polyeval1 = funspace1.funeval(doftype1,1,1,1)
+    print(polyeval1)
+    polyeval2 = funspace1.funeval(doftype2,1,1,1)
+    print(polyeval2)
+#
 #    dimension2 = 1
 #    polytopetype2 = 'cube'
 #    dofnumber2 = 6
