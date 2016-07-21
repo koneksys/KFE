20c88d31797a4d8a0facb92ae24d90e033f398cb
Jerome <jerome@git.de>
2016-07-21 16:54:17 +0100

    define model

---
+++ FEA_model/mesh.py
@@ -0,0 +1,44 @@
+
+class Nodelist:
+
+    pass
+
+Facetoedge=[1,[1,2,3]]
+Edgetonode=[1,[0,1],2,[1,2],3,[2,0]]
+Nodeinf=
+
+class Mesh:
+    def __init__(self, meshelements):
+        self.dimension
+        self.nbnode
+        self.nbedge
+        self.nbface
+        self.nbvolume
+    pass
+
+
+class MeshElements:
+    def __init__(self,MeshElementslist):
+        self.dimension
+    pass
+
+
+class Node(MeshElements):
+    def __init__(self,MeshElementslist):
+        self.dimension=0
+    pass
+
+
+class Edge(MeshElements):
+    def __init__(self,MeshElementslist):
+        self.dimension=1
+        pass
+
+class Nodeindex:
+    pass
+
+
+class Coordinate:
+    pass
+
+
